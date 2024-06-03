50f99bb1320470a56cf88dc7d550efa95a0c115e
ReXxX-69 <kacper852850@gmail.com>
2024-06-03 22:29:06 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,6 +6,6 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_hello_world(client):
+def test_83322(client):
     rv = client.get('/')
-    assert rv.data == b'Hello, World!'
+    assert rv.data == b'Kacper_Jamrozinski_83322'
