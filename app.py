e1b9ce303cb3a609d4dcb1aa3c925b295ce84271
ReXxX-69 <kacper852850@gmail.com>
2024-06-03 22:29:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Kacper_Jamroziński_83322'
+    return 'Kacper_Jamrozinski_83322'
 
 if __name__ == '__main__':
     app.run(debug=True)
